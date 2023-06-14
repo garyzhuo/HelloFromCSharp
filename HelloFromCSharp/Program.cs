9d4a4198ca8bb3d18f6e8359f6795998c3e7adeb
garyzhuo <gary_zhuo@hotmail.com>
2023-06-14 09:52:59 -0500

    Starting Bethany's Pie Shop Project

--- HelloFromCSharp/Program.cs
+++ HelloFromCSharp/Program.cs
@@ -1,9 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+
+
+
 Console.WriteLine("Hello everybody");
 
+Console.WriteLine("Please enter your first name: ");
+string firstName = Console.ReadLine();
 
-Console.WriteLine("Please enter your name:");
+Console.WriteLine("Please enter your last name: ");
+string lastName = Console.ReadLine();
+
+Console.WriteLine("Hello " + firstName + "" + lastName);
 
 
-string name = Console.ReadLine();
-Console.WriteLine("Hello " + name);
